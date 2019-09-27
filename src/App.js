78ac0eb530266628c531ff85096be5669830791c
Matthew Jaikaran <mattjaikaran@gmail.com>
2019-09-27 16:07:46 -0400

    updated with shop page, collection preview, collection item components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
-import Homepage from './pages/Homepage'
+import Homepage from './pages/homepage/Homepage'
+import ShopPage from './pages/shop/Shop'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   )
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Homepage.scss'
-import Directory from '../components/directory/Directory'
+import Directory from '../../components/directory/Directory'
 
 const Homepage = () => (
   <div className="homepage">
