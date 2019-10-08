5f88605ce00cffb417a1c873d1f0937432a99da4
Matthew Jaikaran <mattjaikaran@gmail.com>
2019-10-08 15:48:39 -0400

    moved directory and shop data into its own reducer, created respective selectors and updated affected components with new redux flow for directory/shop

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -6,11 +6,17 @@ import { selectCollection } from '../../redux/shop/shopSelectors'
 
 import './Collection.scss'
 
-const CollectionPage = ({ match }) => (
-  <div className="collection-page">
-    <h2>{match.params.collectionId}</h2>
-  </div>
-)
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => <CollectionItem key={item.id} item={item} />)}
+      </div>
+    </div>
+  )
+}
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
