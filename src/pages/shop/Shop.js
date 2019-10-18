83dfee61aecb70b43ff7a13e5d4a7c135c5415ba
Matthew Jaikaran <mattjaikaran@gmail.com>
2019-10-18 15:07:47 -0400

    updated collectionRef to use get call instead of snapshot

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -21,7 +21,8 @@ class ShopPage extends Component {
     const { updateCollections } = this.props
     const collectionRef = firestore.collection('collections')
 
-    collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get()
+    .then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
       this.setState({ loading: false })
