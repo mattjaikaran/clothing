f153c0530937bb18eb01d4f0d0bdadc1087aaa11
Matthew Jaikaran <mattjaikaran@gmail.com>
2019-11-22 15:25:29 -0500

    added React app into client folder, added express server to handle stripe payments

--- client/src/components/stripe-button/StripeButton.js
+++ client/src/components/stripe-button/StripeButton.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
   const publishableKey = 'pk_test_1XMemRBuZH4LUNMOolEnvbPn'
 
-  const onToken = token => {
-    console.log(token)
-    alert('Payment Successful')
+  const onToken = async (token) => {
+    try {
+      const response = await axios({
+        url: 'payment',
+        method: 'post',
+        data: {
+          amount: priceForStripe,
+          token
+        }
+      })
+      alert('Payment Successful', response)
+    } catch (err) {
+      console.log('Payment Error: ', JSON.parse(err))
+      alert('There was an issue with your payment. Please use valid info.')
+    }
   }
 
   return (
     <StripeCheckout
       label="Pay Now"
-      name="Matt Clothing Ltd"
+      name="Matt Clothing"
       billingAddress
       shippingAddress
       image="http://svgshare.com/i/CUz.svg"
