7d46a9e5eefe03365a00ec4db2071357de56c10a
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-03-26 16:47:59 -0400

    updated redux for cart to store in firebase

--- client/src/redux/cart/cartSagas.js
+++ client/src/redux/cart/cartSagas.js
@@ -1,18 +1,55 @@
 import { all, call, takeLatest, put } from 'redux-saga/effects'
 import UserActionTypes from '../user/userTypes'
-import { clearCart } from './cartActions'
+import { clearCart, setCartFromFirebase } from './cartActions'
+import CartActionTypes from './cartTypes'
 
 
 export function* clearCartOnSignOut() {
   yield put(clearCart())
 }
 
+export function* updateCartInFirebase() {
+  const currentUser = yield select(selectCurentUser)
+  if (currentUser) {
+    try {
+      const cartRef = yield getUserCartRef(currentUser.id)
+      const cartItems = yield select(selectCartItems)
+      yield cartRef.update({ cartItems })
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+}
+
+export function* checkCartFromFirebase({ payload: user }) {
+  const cartRef = yield getUserCartRef(user.id)
+  const cartSnapshot = yield cartRef.get()
+  yield put(setCartFromFirebase(cartSnapshot.data().cartItems))
+}
+
+export function* onUserSignIn() {
+  yield takeLatest(UserActionTypes.SIGN_IN_SUCCESS, checkCartFromFirebase)
+}
+
 export function* onSignOutSuccess() {
   yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut)
 }
 
+export function* onCartChange() {
+  yield takeLatest(
+    [
+      CartActionTypes.ADD_ITEM,
+      CartActionTypes.REMOVE_ITEM,
+      CartActionTypes.CLEAR_ITEM_FROM_CART,
+    ],
+    updateCartInFirebase
+  )
+}
+
 export function* cartSagas() {
   yield(all([
-    call(onSignOutSuccess)
+    call(onSignOutSuccess),
+    call(onCartChange),
+    call(onUserSignIn)
   ]))
 }
