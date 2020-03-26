0853c5ec5802c6c63f84bebc71988042917039ce
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-03-26 15:36:48 -0400

    updated error in server file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(compression())
-app.use(enforce.HTTPS({ trustProtoHeader: true })
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -31,8 +31,8 @@ app.listen(PORT, (error) => {
   console.log(`Listening on Port ${PORT}`)
 })
 
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
+app.get('/serviceWorker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'serviceWorker.js'))
 })
 
 
