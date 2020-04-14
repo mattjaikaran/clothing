6769ee6f31a03336877cc09d88c516fd5cc608c7
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-04-14 15:58:49 -0400

    updated slider styles

--- client/src/components/hero-slider/HeroSlider.js
+++ client/src/components/hero-slider/HeroSlider.js
@@ -2,23 +2,48 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Carousel from 'react-material-ui-carousel'
 import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
 import './HeroSlider.css'
 
 const useStyles = makeStyles({
   carouselImg: {
     width: '100%'
+  },
+  carouselImgContainer: {
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '500px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: '20vh',
+    color: 'white',
+    opacity: '0.9'
+  },
+  cta: {
+    textAlign: 'center',
+    alignContent: 'center',
+    margin: '0 auto',
+    maxWidth: '200px'
   }
 })
 
 const Item = (props) => {
   const classes = useStyles()
-  const { name, description, imgSrc } = props.item
+  const { name, description, imgSrc, link, linkCopy } = props.item
   return (
-    <div className="carouselImgContainer">
-      <img 
-        className={classes.carouselImg} 
-        src={imgSrc} 
-        alt={name} />
+    <div 
+      className={classes.carouselImgContainer} 
+      style={{backgroundImage: `url(${imgSrc})`}}>
+      <h3>{name}</h3>
+      <Button
+        variant="contained" 
+        color="primary"
+        className={classes.cta}
+        to={link}>
+        {linkCopy}
+      </Button>
     </div>
   )
 }
@@ -26,13 +51,17 @@ const Item = (props) => {
 const HeroSlider = () => {
   const items = [
     {
-      name: "Random Name #1",
-      imgSrc: "https://firebasestorage.googleapis.com/v0/b/clothing-c8c08.appspot.com/o/andrew-neel-HqtYwlY9dxs-unsplash.jpg?alt=media&token=3937b169-d6c2-4b17-950a-c13850fe52bc"
+      name: "Up to 90% off on designer jackets",
+      imgSrc: "https://firebasestorage.googleapis.com/v0/b/clothing-c8c08.appspot.com/o/andrew-neel-HqtYwlY9dxs-unsplash.jpg?alt=media&token=3937b169-d6c2-4b17-950a-c13850fe52bc",
+      linkCopy: "Shop Outwear",
+      link: "/shop/jackets",
     },
     {
       name: "Hello World!",
       description: "Probably the most random thing you have ever seen!",
-      imgSrc: "https://firebasestorage.googleapis.com/v0/b/clothing-c8c08.appspot.com/o/zdenek-machacek-lmZtT6q8tt0-unsplash.jpg?alt=media&token=6e56df04-1d4a-4ec4-a51d-baef07b374e1"
+      imgSrc: "https://firebasestorage.googleapis.com/v0/b/clothing-c8c08.appspot.com/o/zdenek-machacek-lmZtT6q8tt0-unsplash.jpg?alt=media&token=6e56df04-1d4a-4ec4-a51d-baef07b374e1",
+      linkCopy: "Explore Accessories",
+      link: "/shop/hats",
     }
   ]
   return (
