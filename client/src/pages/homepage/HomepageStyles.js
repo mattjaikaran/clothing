0c9de2ee30cc0beefa2f73a8c7838f622036074f
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-04-14 17:11:59 -0400

    fixedtypo

--- client/src/pages/homepage/HomepageStyles.js
+++ client/src/pages/homepage/HomepageStyles.js
@@ -4,7 +4,7 @@ export const HomepageContainer = styled.div`
   align-items: center;
   padding: 10px;
   @media screen and (min-width: 768px) and (max-width: 959px) {
-    padding: 20px 0px;
+    padding: 0 20px;
   }
   @media screen and (max-width: 767px) {
     padding: 0 15px;
