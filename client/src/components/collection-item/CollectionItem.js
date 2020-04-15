c0c52e5d61ffdd515635c8dff2257d061775362f
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-04-14 21:11:37 -0400

    updated collection item buttons

--- client/src/components/collection-item/CollectionItem.js
+++ client/src/components/collection-item/CollectionItem.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addItem } from '../../redux/cart/cartActions'
-
 import {
   CollectionItemContainer,
   CollectionFooterContainer,
@@ -10,11 +9,38 @@ import {
   NameContainer,
   PriceContainer
 } from './CollectionItemStyles'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
 
-import CustomButton from '../custom-button/CustomButton'
+const useStyles = makeStyles(theme => ({
+  btn: {
+    width: '80%',
+    backgroundColor: 'black',
+    color: 'white',
+    opacity: 0.7,
+    height: '40px',
+    position: 'absolute',
+    marginTop: '250px',
+    display: 'none',
+    '&:hover': {
+      transition: 'background-color 0.5s ease',
+      backgroundColor: 'white',
+      color: 'black',
+      display: 'block'
+    },
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+      opacity: 0.9,
+      minWidth: 'unset',
+      padding: '0 10px',
+    }
+  }
+}))
+
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item
+  const classes = useStyles()
   return (
     <CollectionItemContainer>
       <BackgroundImage className='image' imageUrl={imageUrl} />
@@ -22,9 +48,12 @@ const CollectionItem = ({ item, addItem }) => {
         <NameContainer>{name}</NameContainer>
         <PriceContainer>${price}</PriceContainer>
       </CollectionFooterContainer>
-      <AddButton onClick={() => addItem(item)} inverted>
-        Add to cart
-      </AddButton>
+      <Button 
+        className={classes.btn}
+        variant="outlined" 
+        onClick={() => addItem(item)}>
+        Add To Cart
+      </Button>
     </CollectionItemContainer>
   )
 }
